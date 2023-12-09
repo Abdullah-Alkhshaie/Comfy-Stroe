3ae32428e67b145c4b443af32dd31e4fc7739eac
Abdullah <125403129+Abdullah-Alkhshaie@users.noreply.github.com>
2023-12-09 14:45:32 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       <Header />
       <Navbar />
 
-      <Routes basename="/Comfy-Stroe">
+      <Routes basename="/">
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/signin" element={<SignIn />} />
